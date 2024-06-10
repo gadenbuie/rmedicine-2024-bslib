3dd02571b01c0d3ea9c9c3636f345095e48bcbc7
Garrick Aden-Buie <garrick@adenbuie.com>
2024-06-10 08:15:15 -0400

    Compare cost to earnings in exercise 2

--- exercises/02_solution_app.R
+++ exercises/02_solution_app.R
@@ -57,12 +57,12 @@ ui <- page_sidebar(
     )
   ),
   card(
-    card_header("Cost vs Size"),
+    card_header("Cost vs Earnings"),
     layout_sidebar(
       sidebar = sidebar(
         input_group_by
       ),
-      plotlyOutput("plot_cost_size")
+      plotlyOutput("plot_cost_earnings")
     ),
     full_screen = TRUE
   )
@@ -85,8 +85,7 @@ server <- function(input, output, session) {
       filter(deg_predominant == input$deg_predominant)
 
     scorecard |>
-      semi_join(school_filter, by = "id") |>
-      filter(academic_year == max(academic_year))
+      semi_join(school_filter, by = "id")
   })
 
   plotly_cleaner <- function(x) {
@@ -95,8 +94,14 @@ server <- function(input, output, session) {
       layout(margin = list(l = 0, r = 0, b = 0))
   }
 
+  filter_recent_complete_year <- function(scorecard, column) {
+    academic_year <- scorecard[!is.na(scorecard[[column]]), ]$academic_year
+    scorecard |> filter(academic_year == !!max(academic_year))
+  }
+
   output$plot_rate_admissions <- renderPlotly({
     r_scorecard() |>
+      filter_recent_complete_year("rate_admissions") |>
       filter(!is.na(rate_admissions)) |>
       plot_ly(x = ~rate_admissions, type = "histogram") |>
       layout(xaxis = list(title = "Rate")) |>
@@ -105,31 +110,34 @@ server <- function(input, output, session) {
 
   output$plot_rate_completion <- renderPlotly({
     r_scorecard() |>
+      filter_recent_complete_year("rate_completion") |>
       filter(rate_completion > 0) |>
       plot_ly(x = ~rate_completion, type = "histogram") |>
       layout(xaxis = list(title = "Rate")) |>
       plotly_cleaner()
   })
 
-  output$plot_cost_size <- renderPlotly({
+  output$plot_cost_earnings <- renderPlotly({
     r_scorecard() |>
+      filter_recent_complete_year("amnt_earnings_med_10y") |>
       left_join(school, by = "id") |>
       plot_ly(
         x = ~cost_avg,
-        y = ~n_undergrads,
+        y = ~amnt_earnings_med_10y,
         color = ~get(input$group_by),
         hoverinfo = "text",
         text = ~paste0(
           "School: ", name, "<br>",
           "Undergrads: ", scales::number(n_undergrads, big.mark=","), "<br>",
-          "Avergae Cost: ", scales::dollar(cost_avg), "<br>",
+          "Average Cost: ", scales::dollar(cost_avg), "<br>",
+          "Median Earnigns: ", scales::dollar(amnt_earnings_med_10y), "<br>",
           "Admissions Rate: ", scales::percent(rate_admissions), "<br>",
           "Completion Rate: ", scales::percent(rate_completion)
         )
       ) |>
       layout(
         xaxis = list(title = "Average Cost"),
-        yaxis = list(title = "Number of Undergrads")
+        yaxis = list(title = "Median Earnings")
       )
   })
 }
